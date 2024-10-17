945d21b6851040f533f433c3a3dc0b3d9d064d4a
JeanTole <jeantole27@gmail.com>
2024-10-17 17:29:45 -0500

    prueba de funcion y se agrego la view historial_equipo

--- app/src/main/java/com/example/app_inventario_arquitectura/MainActivity.kt
+++ app/src/main/java/com/example/app_inventario_arquitectura/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.app_inventario_arquitectura
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import android.widget.EditText
+import android.content.Context
+import android.content.Intent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.view.inputmethod.InputMethodManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +23,24 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
-    fun showToast(){
-        Toast.makeText(this,"Correo o contraseña Invalida",Toast.LENGTH_LONG).show()
+    fun showToast(mensaje:String){
+        Toast.makeText(this,mensaje,Toast.LENGTH_LONG).show()
     }
     fun ingresar(view:View){
+        val textUsuario = findViewById<EditText>(R.id.editTextUsuario)
+        val textPassword = findViewById<EditText>(R.id.editTextPassword)
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+        val email = textUsuario.text.toString().trim()
+        val password = textPassword.text.toString().trim()
 
+        if(email.isEmpty() || password.isEmpty())  {
+            showToast("El usuario y la contraseña no pueden estar vacíos")
+        } else if  (!isValidEmail(email)) {
+            showToast("Correo inválido.")
+        } else {
+            showToast("Puede Ingresar")
+        }
     }
+    private fun isValidEmail(email: String): Boolean { return email.contains("@")}
 }
