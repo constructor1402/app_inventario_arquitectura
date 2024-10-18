9b748c6042bbd7afb0001a5648a39792186ab395
usuario1 <usuario1@M2-305-15-11374.utadeo.loc>
2024-10-17 19:39:56 -0500

    creación de las activity y rehaciendo los layout

---
+++ app/src/main/java/com/example/app_inventario_arquitectura/EliminarEquipo.kt
@@ -0,0 +1,20 @@
+package com.example.app_inventario_arquitectura
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class EliminarEquipo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_eliminar_equipo)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
