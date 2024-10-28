16adc50443712975c599f6b01fbd7322e7c433be
constructor1402 <cristianestudiante15@gmail.com>
2024-10-28 00:09:49 -0500

    actualizacion de panel de control y fragments crear editar y eliminar

--- app/src/main/java/com/example/app_inventario_arquitectura/crearFragment.kt
+++ app/src/main/java/com/example/app_inventario_arquitectura/crearFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.cardview.widget.CardView
-import androidx.fragment.app.commit
 import com.example.androidmaster.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -16,33 +14,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [panelDeControlFragment.newInstance] factory method to
+ * Use the [crearFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class panelDeControlFragment : Fragment(R.layout.fragment_panel_de_control) {
+class crearFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val cardViewOpcion1: CardView = view.findViewById(R.id.cardGestionEquipos)
-
-        cardViewOpcion1.setOnClickListener{
-            parentFragmentManager.commit {
-                replace(R.id.fragmentContainer, crearFragment())
-                addToBackStack(null)
-            }
-        }
-
-
-    }
-
-
-
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,7 +35,7 @@ class panelDeControlFragment : Fragment(R.layout.fragment_panel_de_control) {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_panel_de_control, container, false)
+        return inflater.inflate(R.layout.fragment_crear, container, false)
     }
 
     companion object {
@@ -67,12 +45,12 @@ class panelDeControlFragment : Fragment(R.layout.fragment_panel_de_control) {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment panelDeControlFragment.
+         * @return A new instance of fragment gestionEquiposFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            panelDeControlFragment().apply {
+            crearFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
